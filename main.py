f2171a72a3343ff5936866404d1f5595b55d819c
mufaddalhatim53 <mufaddalhatim53@gmail.com>
2023-11-04 11:45:47 +0500

    added sign up functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, json
 from flask import render_template, redirect, url_for, request, session, jsonify
-from config import app
+from config import app, mysql
 from datetime import datetime
 
 #################### UPDATED TO CHECK PULL PUSH GIT ###############
@@ -19,7 +19,35 @@ def login():
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template('signup.html')
+    if request.method != 'POST':
+        return render_template('signup.html')
+    data = request.get_json()
+    print(data, request.get_data())
+
+@app.route('/signup_farmer',methods=["GET","POST"])
+def signup_farmer():
+    
+    if request.method != 'POST':
+        return ''
+    
+    data = request.get_json()['data']
+    user_data = {}
+    
+    for i in data:
+        user_data[i['name']] = str(i['value'])
+
+    if user_data['password'] != user_data['reenter-password']:
+        return jsonify({"success":False, "message":"Passwords do not match"})
+    
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO useraccount (username, password, role) values (%s, %s, %s)", (user_data['username'], user_data['password'], user_data['category']))
+    mysql.connection.commit()
+    user_id = cur.lastrowid
+    if user_data['category'] == 'farmer':
+        cur.execute("INSERT INTO farmer (Name, Location, ContactPhone, ContactEmail, FarmSize, FarmingExperience, UserAccountID) VALUES (%s, %s, %s, %s, %s, %s, %s)", (user_data['username'], '', user_data['phone'], user_data['email'], '', '', user_id))
+        mysql.connection.commit()
+
+    return jsonify({"success":True, 'message':'User created successfully', "category":user_data['category']})
 
 @app.route("/products", methods=['GET', 'POST'])
 def products():
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+let baseUrl = new URL(window.location.href);
+baseUrl = `${baseUrl.protocol}//${baseUrl.hostname}:${baseUrl.port}`;
+
 document.getElementById('searchInput').addEventListener('keyup', function() {
     const searchTerm = this.value.toLowerCase();
     const productCards = document.querySelectorAll('.product-card');
@@ -21,3 +24,26 @@ function signup() {
 function login() {
     window.location.href = "/login";
 }
+
+$("#signup_form #submit").click(async function(event) {
+    event.preventDefault();
+    var data = $("#signup_form").serializeArray();
+    console.log({data});
+    data = {
+        "data": data
+    }
+    await axios({
+        method: "POST",
+        url: baseUrl + "/signup_farmer",
+        data: data
+    }).then((response) => {
+        console.log(response);
+        if (response.data.status == "success") {
+            window.location.href = "/login";
+        } else {
+            alert(response.data.message);
+        }
+    }).catch((error) => {
+        console.log(error);
+    });
+})
